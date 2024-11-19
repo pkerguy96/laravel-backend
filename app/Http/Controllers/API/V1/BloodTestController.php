6ad5525b838442c8ffb0ec597e52bbdaf68c0b65
pkerguy96 <aymenelkor@hotmail.com>
2024-11-19 22:12:11 +0100

    update 2

--- app/Http/Controllers/API/V1/BloodTestController.php
+++ app/Http/Controllers/API/V1/BloodTestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreBloodTestRequest;
 use App\Models\Bloodtest;
+use App\Models\Patient;
 use App\Models\WaitingRoom;
 use App\Traits\HttpResponses;
 use Carbon\Carbon;
@@ -40,7 +41,8 @@ class BloodTestController extends Controller
                     'blood_test' => $test,
                 ]);
             }
-            $waiting =   WaitingRoom::where('patient_id', $request->patient_id)->first();
+            $waiting =  WaitingRoom::where('patient_id', $request->patient_id)->first();
+            $patient = Patient::where('id', $request->patient_id)->first();
             if ($waiting) {
                 $waiting->update([
                     'status' => 'current'
@@ -50,10 +52,11 @@ class BloodTestController extends Controller
                     'status' => 'current',
                     'patient_id'
                     => $request->patient_id,
-                    'entry_time' => Carbon::now()
+                    'entry_time' => Carbon::now(),
+
                 ]);
             }
-            return $this->success(null, 'Test sanguin enregistré avec succès', 200);
+            return $this->success($patient, 'Test sanguin enregistré avec succès', 200);
         } catch (\Throwable $th) {
             return $this->error($th->getMessage(), 'oops something went wrong', 500);
         }
